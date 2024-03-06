0ce50936fa7ae01b8d485a9c96bd64465a30bd8b
Karthik-beta <karthikpar@protonmail.com>
2024-03-06 10:44:25 +0530

    1.06.03.24.2

---
+++ weekly_exe.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    return output, error
+
+def main():
+    # Step 1: Navigate to the specified directory
+    target_directory = r"C:\GETINSOLUTION\V2\sael-backend"
+    os.chdir(target_directory)
+
+    # Step 2: Run the specified command
+    python_exe_path = r"C:\Users\Dell\AppData\Local\Programs\Python\Python311\python.exe"
+    command_to_run = f'{python_exe_path} manage.py import_weekly "C:\\Users\\Dell\\Desktop\\WIP\\weekly_target.xlsx"'
+    
+    output, error = run_command(command_to_run)
+
+    # Display the output and error, if any
+    print("Output:", output.decode("utf-8"))
+    print("Error:", error.decode("utf-8"))
+
+if __name__ == "__main__":
+    main()
